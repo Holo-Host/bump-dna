b79190da46bcfd43a65533b1b6663d37f17f6950
PJ <pj@imagine-nyc.com>
2020-05-06 10:46:17 +0200

    create bump-dna shell script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bump-dna
-Bash script for quick altering of hashspace of selfhosted DNAs on HPOS
+Bash script for quick altering of hashspace of selected DNA on HPOS
+
+usage: bump-dna [-ih] [-u]
+    -i id of dna
+    -h hash of dna before bump (overwrites -i)
+    -u new uuid (optional, skipping will print current uuid)
+
+If successful the script will:
+- copy dna to a new file inside `dna/` folder
+- change uuid property inside of it
+- get new hash of dna and rename dna file to `new_hash.dna.json`
+- print new hash
+- update holochain config file and restart `holochain-conductor.service` for changes to take effect
---
+++ bump-dna.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+# Simple shell script for altering hashspace of self hosted DNAs
+# type bump-dna for help
+##
+
+CONFIG_FILE="/var/lib/holochain-conductor/conductor-config.toml"
+DNA_DIR="/var/lib/holochain-conductor/dnas/"
+
+[[ -f $CONFIG_FILE ]] || { echo "$CONFIG_FILE is not a file." >&2;exit 1;}
+
+hash_from_id(){
+    local a="\"$1\"" # wrap id in double quotes
+    echo "$( awk -v my_id=$a '
+        /\[\[dnas\]\]/,/^$/ {
+            if ($1 == "hash") hash=$3
+            if ($1 == "id") id=$3
+            if ($0 == "" && id == my_id) print hash
+        }' $CONFIG_FILE | xargs)" # xargs removes double quotes from around hash
+}
+
+file_from_hash(){
+    local a="\"$1\"" # wrap hash in double quotes
+    echo "$( awk -v my_hash=$a '
+        /\[\[dnas\]\]/,/^$/ {
+            if ($1 == "hash") hash=$3
+            if ($1 == "file") file=$3
+            if ($0 == "" && hash == my_hash) print file
+        }' $CONFIG_FILE | xargs)" # xargs removes double quotes from around file
+}
+
+if [ $# -eq 0 ]
+    then
+        echo "usage: bump-dna [-ih] [-u] "
+        echo "  -i id of dna"
+        echo "  -h hash of dna before bump (overwrites -i)"
+        echo "  -u new uuid (optional, skipping will print current uuid)"
+        exit 0
+fi
+
+while getopts i:h:u: OPTION
+do
+    case $OPTION in
+        i)
+            id=${OPTARG}
+            hash_id=$( hash_from_id $id )
+            optflag=1
+            [[ -n $hash_id ]] || { echo "There is no dna with id $id" >&2;exit 1;}
+            ;;
+        h)  
+            hash_h=${OPTARG}
+            optflag=1
+            ;;
+        u)
+            uuid=${OPTARG}
+            ;;
+        /?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+    esac
+done
+
+# Error checks
+[[ -n $optflag ]] || { echo "Either -i or -h is required for identification of DNA" >&2;exit 1;}
+hash=${hash_h:-${hash_id}} # -h overwrites -i
+dna_path=$( file_from_hash $hash)
+[[ -n $dna_path ]] || { echo "There is no dna with hash $hash" >&2;exit 1;}
+[[ -f $dna_path ]] || { echo "Failed to read from file $dna_path" >&2;exit 1;}
+
+# If no uuid provided print current uuid
+[[ -n $uuid ]] || { echo "Current uuid is "$( grep 'uuid' ${dna_path} | awk '{print $2}' | sed 's/,$//' | xargs );exit 0;}
+
+tmp_path=$(mktemp -u)
+cp $dna_path $tmp_path # will it create any permission conflicts?
+
+# change uuid in a new file
+sed -i "/uuid/c\  \"uuid\": \"$uuid\"\," $tmp_path
+
+# calculate new hash
+new_hash=$(hc hash -p $tmp_path | sed -n 2p | awk '{print $3}')
+
+# copy updated dna to dnas/
+new_path=$DNA_DIR"/"$new_hash".dna.json"
+[[ -d $DNA_DIR ]] || { echo "Creating dir $DNA_DIR"; mkdir $DNA_DIR;}
+mv $tmp_path $new_path
+
+# sed new_hash and new_path
+sed -i "/hash.*$hash/c\hash = \'$new_hash\'" $CONFIG_FILE
+sed -i "/file.*$dna_path/c\file = \'$new_path\'" $CONFIG_FILE
+
+echo ""
+echo "new_hash: $new_hash"
+echo ""
+echo "restarting holochain-conductor.service..."
+systemctl restart holochain-conductor.service
+
+exit 0
